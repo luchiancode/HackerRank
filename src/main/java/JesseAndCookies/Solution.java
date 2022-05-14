d7ea50bca5d38ba7bb50f6ca5af4aa5d2dbca95c
DESKTOP-2RGFJTE\Luchian <ionica.luchian@hotmail.com>
2022-05-14 21:13:35 +0300

    Jesse and Cookies

---
+++ src/main/java/JesseAndCookies/Solution.java
@@ -0,0 +1,75 @@
+package JesseAndCookies;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'cookies' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY A
+     */
+
+    public static int cookies(int k, List<Integer> A) {
+        // Write your code here
+        int numberOfPermutations = 0;
+
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.addAll(A);
+
+        while(pq.peek() < k && pq.size() >= 2){
+            pq.add(pq.remove() + 2*pq.remove());
+            numberOfPermutations++;
+        }
+
+        if(pq.peek() < k)
+            numberOfPermutations = -1;
+
+
+
+
+        return numberOfPermutations;
+
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.cookies(k, A);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
